6176a6bed0725ac088ad64fe59b1f735c90a314a
Christopher Lewis <chris.stephen.lewis@gmail.com>
2023-09-25 15:24:50 -0400

    WIP navbar custom

--- src/components/common/header/Navigation.tsx
+++ src/components/common/header/Navigation.tsx
@@ -2,24 +2,29 @@ import { Link } from 'react-router-dom';
 
 function Navigation() {
     return (
-        <nav className="bg-white border-gray-200 dark:bg-gray-900">
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
+        <nav className=" bg-gray-900 flex justify-between p-3 max-w-screen-xl flex-wrap items-center">
+            <div className="flex">
+                <Link to="/" className="flex">
+                    <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-8" />
+                    <span className="text-gray-100 text-2xl whitespace-nowrap font-semibold">
+                        Page Name
+                    </span>
+                </Link>
+            </div>
+            <ul className="flex">
+                <li className="text-gray-200 hover:text-yellow-500 pl-3 pr-3 py-2">
                     <Link to="/about">About</Link>
                 </li>
-                <li>
+                <li className="text-gray-200 hover:text-yellow-500 pl-3 pr-3 py-2">
                     <Link to="/experience">Experience</Link>
                 </li>
-                <li>
+                <li className="text-gray-200 hover:text-yellow-500 pl-3 pr-3 py-2">
                     <Link to="/work">Work</Link>
                 </li>
-                <li>
+                <li className="text-gray-200 hover:text-yellow-500 pl-3 pr-3 py-2">
                     <Link to="/blog">Blog</Link>
                 </li>
-                <li>
+                <li className="text-gray-200 hover:text-yellow-500 pl-3 pr-3 py-2">
                     <Link to="/contact">Contact</Link>
                 </li>
             </ul>
